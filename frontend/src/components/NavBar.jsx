e358b411f45d7c56ed3e351e40b9064bca2c9c79
Jasoonii <jason.ddang1@gmail.com>
2025-02-20 01:12:24 -0600

    Finished implementing navBar from Figma design

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,30 @@
 import "../css/NavBar.css";
+import { Karla } from "next/font/google";
+import Image from "next/image";
+
+//Load Karla Font
+const karla = Karla({ subsets:["latin"], weight:["700"] });
 
 export default function NavBar() {
     return (
         <nav className="navBar">
             <div className="container">
-                <a className="logo">In-n-Out</a>
+                <a className="logo">
+                    <Image src="/restaurantLogo.png" alt="logo" width={150} height={150} />
+                </a>
                 
                 <ul className="navLinks">
-                    <li><a>Home</a></li>
-                    <li><a>Menu</a></li>
-                    <li><a>Support</a></li>
+                    <li><a>HOME</a></li>
+                    <li><a>MENU</a></li>
+                    <li><a>ORDER</a></li>
+                    <li><a>LOGIN</a></li>
                 </ul>
+
+                
             </div>
+            <div className="thinLine"></div>
+            <div className="whiteSpace"></div>
+            <div className="thickLine"></div>
         </nav>
     );
 }
